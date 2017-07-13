ce7aa8a928419273bd8fd5e41ae7998e56ce5b4b
Enrico Sada <enrico@sada.io>
2017-07-13 14:38:24 +0200

    f

---
+++ src/WinTest/MainWindow.xaml.fs
@@ -0,0 +1,12 @@
+namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MainView = XAML<"MainWindow.xaml">
+
+type MainViewModel() as self = 
+    inherit ViewModelBase()    
